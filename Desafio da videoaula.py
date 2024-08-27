a8527583e638a02a4691760357e788f0074ac7dc
Vitor Pinheiro Guimarães <vitorguimap@gmail.com>
2024-08-27 00:35:19 -0300

    .

--- Desafio da videoaula.py
+++ Desafio da videoaula.py
@@ -8,7 +8,7 @@ delta = ((b ** 2) - 4 * a * c)
 
 if delta == 0:
     raiz1 = (-b + math.sqrt(delta)) / (2 * a)
-    print("a raiz desta equação é ", raiz1)
+    print("a raiz desta equação é",raiz1)
 
 else:
     if delta < 0:
@@ -16,7 +16,7 @@ else:
     else:
         raiz1 = (-b + math.sqrt(delta)) / (2 * a)
         raiz2 = (-b - math.sqrt(delta)) / (2 * a)
-        if raiz1 < raiz2:
-             print("as raízes da equação são",raiz2,"e ",raiz1)
+        if raiz1 > raiz2:
+             print("as raízes da equação são",raiz2,"e",raiz1)
         else:
-             print("as raízes da equação são",raiz1,"e ",raiz2)
+             print("as raízes da equação são",raiz1,"e",raiz2)
