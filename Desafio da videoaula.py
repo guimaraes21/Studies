a6b63be5023adf7dd8fc3d230bd0acf0dc5509ad
Vitor Pinheiro Guimarães <vitorguimap@gmail.com>
2024-08-27 00:22:16 -0300

    .

--- Desafio da videoaula.py
+++ Desafio da videoaula.py
@@ -1,22 +1,22 @@
+a = float(int(input("Digite o valor de A: ")))
+b = float(int(input("Digite o valor de B: ")))
+c = float(int(input("Digite o valor de C: ")))
+
 import math
 
-# Recebe os coeficientes a, b e c da equação
-a = float(input("Digite o valor de a: "))
-b = float(input("Digite o valor de b: "))
-c = float(input("Digite o valor de c: "))
+delta = ((b ** 2) - 4 * a * c)
 
-# Calcula o discriminante
-delta = b**2 - 4*a*c
-
-# Verifica o valor do discriminante para determinar o tipo de raízes
-if delta < 0:
-    print("Esta equação não possui raízes reais")
-elif delta == 0:
-    raiz = -b / (2 * a)
-    print(f"A raiz dupla desta equação é {raiz:.1f}")
-else:
+if delta == 0:
     raiz1 = (-b + math.sqrt(delta)) / (2 * a)
-    raiz2 = (-b - math.sqrt(delta)) / (2 * a)
-    if raiz1 > raiz2:
-        raiz1, raiz2 = raiz2, raiz1
-    print(f"As raízes da equação são {raiz1:.1f} e {raiz2:.1f}")
+    print("a raiz desta equação é ", raiz1)
+
+else:
+    if delta < 0:
+        print("esta equação não possui raízes reais")
+    else:
+        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+        raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+        if raiz1 < raiz2:
+             print("as raízes da equação são",raiz2,"e ",raiz1)
+        else:
+             print("as raízes da equação são",raiz1,"e ",raiz2)
